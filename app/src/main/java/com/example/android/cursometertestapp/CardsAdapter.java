9ca55910ee4cce21c5105bb558da8aec9eb07086
dmitry-kotenev <dmitry.kotenev@bk.ru>
2017-10-01 21:51:06 +0300

    Clarify CardsAdapter.

--- app/src/main/java/com/example/android/cursometertestapp/CardsAdapter.java
+++ app/src/main/java/com/example/android/cursometertestapp/CardsAdapter.java
@@ -9,28 +9,26 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 /**
- * Адаптер для списка карточек.
+ * Class for adapter for list of cards.
  */
 
-public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> {
+class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> {
     private ArrayList<SubscribedData.Bank> mListOfBanks;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mBankNameTextView;
-        public TextView mDateAndTimeTextView;
-        public ItemsAdapter mItemsAdapter;
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView mBankNameTextView;
+        TextView mDateAndTimeTextView;
+        ItemsAdapter mItemsAdapter;
 
-        public ViewHolder(CardView oneCard) {
+        ViewHolder(CardView oneCard) {
             super(oneCard);
             mBankNameTextView = (TextView) oneCard.findViewById(R.id.bank_name);
             mDateAndTimeTextView = (TextView) oneCard.findViewById(R.id.last_update_date_time);
-
             RecyclerView cardRecyclerView = (RecyclerView) oneCard.
                     findViewById(R.id.items_in_card_list);
 
-            // Строка ниже необходима для правильной работы FAB и AppBar когда пользователь
-            // перематывает RecyclerView нажимая на карточку.
-            //
+            // The following line is necessary for right behaviour of FAB and AppBar when the user
+            // scroll the recycler view tapping on card.
             // https://stackoverflow.com/questions/33050907/nested-recyclerview-issue-with-appbarlayout
             cardRecyclerView.setNestedScrollingEnabled(false);
 
@@ -38,7 +36,7 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
             mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
             cardRecyclerView.setLayoutManager(mLayoutManager);
 
-            cardRecyclerView.setHasFixedSize(true); // Возможно, улучшает производительность.
+            cardRecyclerView.setHasFixedSize(true); // Maybe this line leads to better performance.
 
             mItemsAdapter = new ItemsAdapter();
             cardRecyclerView.setAdapter(mItemsAdapter);
@@ -50,7 +48,12 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
      * повторном использовании ViewHolder'а в нём не будет изменяться количество строк,
      * соответственно, не будет вызываться метод Inflate
      *
-     * @param position
+     * All ViewHolders are divided to different types in accordance of the number of rows that they
+     * have. That leads to not changing the number of rows of a ViewHolder while reusing it.
+     * Therefore operation of inflate and finding views by ID are don't invokes while reusing
+     * ViewHolders.
+     *
+     * @param position - position in a RecyclerView.
      * @return - number of exchange rates from one bank that is also considered as a ViewType.
      */
     @Override
@@ -58,7 +61,7 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
         return mListOfBanks.get(position).getQuotations().size();
     }
 
-    public CardsAdapter(ArrayList<SubscribedData.Bank> data) {
+    CardsAdapter(ArrayList<SubscribedData.Bank> data) {
         mListOfBanks = data;
     }
 
@@ -72,8 +75,12 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(CardsAdapter.ViewHolder holder, int position) {
         holder.mBankNameTextView.setText(mListOfBanks.get(position).getName());
-        holder.mDateAndTimeTextView.setText(mListOfBanks.get(position).getQuotation(0).getDateTime()); //set date and time for first quotation temporarily
-
+        // Set date and time from the first quotation as it is temporarily.
+        // In the data structure that is received from API, each quotation record has it's own
+        // update date and time. I define the way how to choose from them later.
+        // From of representation of date and time is needed to be updated too.
+        holder.mDateAndTimeTextView.setText(mListOfBanks.get(position).getQuotation(0).
+                getDateTime());
         holder.mItemsAdapter.setData(mListOfBanks.get(position).getQuotations());
     }
 
