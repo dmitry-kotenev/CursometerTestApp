50495d6c63ca4646b0309b3612c7baa9d2a97f94
dmitry-kotenev <dmitry.kotenev@bk.ru>
2017-10-01 22:18:49 +0300

    Clarify CurrenciesFragmentPagerAdapter.

--- app/src/main/java/com/example/android/cursometertestapp/CurrenciesFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/cursometertestapp/CurrenciesFragmentPagerAdapter.java
@@ -6,32 +6,25 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 
 /**
  * Адаптер для ViewPager в котором показыватся курсы валют.
+ *
+ * Adapter for ViewPager quotations are showed in.
  */
 
-public class CurrenciesFragmentPagerAdapter extends FragmentStatePagerAdapter {
+class CurrenciesFragmentPagerAdapter extends FragmentStatePagerAdapter {
 
     private MainActivity mainActivityInstance;
 
 
-    public CurrenciesFragmentPagerAdapter(FragmentManager fm, MainActivity mainActivity) {
+    CurrenciesFragmentPagerAdapter(FragmentManager fm, MainActivity mainActivity) {
         super(fm);
         mainActivityInstance = mainActivity;
     }
-
-
 
     @Override
     public Fragment getItem(int position) {
         CurrenciesFragment resultFragment = new CurrenciesFragment();
         resultFragment.setParams(position);
         return resultFragment;
-    }
-
-    // Стандартный метод getPageTitle не используется, т. к. его использование приводит к появлению
-    // заголовков страниц в TabLayout.
-    public String getFragmentShortTitle(int position) {
-        return mainActivityInstance.getApplicationCurrentSubscribedData().getCurrencyPair(position).
-                getName();
     }
 
     @Override
@@ -42,10 +35,4 @@ public class CurrenciesFragmentPagerAdapter extends FragmentStatePagerAdapter {
         }
         return currData.size();
     }
-
-    // https://stackoverflow.com/questions/28116710/pageradapter-notifydatasetchanged-does-not-refresh-fragments
-//    @Override
-//    public int getItemPosition(Object object){
-//        return PagerAdapter.POSITION_NONE;
-//    }
 }
